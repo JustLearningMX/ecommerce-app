9a04e362c220f3ee688198f7b02a3b8826d0f7f4
Hiram Chavez <virtual.liga@gmail.com>
2023-05-11 20:13:53 -0600

    First config

---
+++ src/main/java/me/hiramchavez/ecommerceapp/controllers/CartsController.java
@@ -0,0 +1,9 @@
+package me.hiramchavez.ecommerceapp.controllers;
+
+import _me.hiramchavez.ecommerceapp.api.CartApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartsController implements CartApi {
+    //first we need to create a cart
+}
